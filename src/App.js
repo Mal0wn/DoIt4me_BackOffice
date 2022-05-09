a04bcd4b7133ed3b2d494b9044a68e6c109739c4
Marine M <malomclt@gmail.com>
2022-05-09 16:50:43 +0200

    Component userListItem Ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import NavBar from "./components/NavBar/NavBar";
+import UsersList from "./pages/Users/UsersList";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+        <div id="block">
+            <Header/>
+            <NavBar/>
+
+        </div>
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/pages/Users/UsersList.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import ItemListUser from '../../components/ItemListUser/ItemListUser'
+
+const UserList = () => {
+
+
+
+
+  return (
+      <div id="userList">
+
+        <ItemListUser/>
+
+      </div>
+  )
+ }
+
+ export default UserList;
