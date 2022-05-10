a74407683cb8bcca767134d4804200b60c83b41e
Marine M <malomclt@gmail.com>
2022-05-10 11:28:08 +0200

    add fake db for test

---
+++ src/db.js
@@ -0,0 +1,543 @@
+
+ export const users =  [
+  {
+    "id": "627a1c6202fd6d0aba708897",
+    "firstName": "Jody",
+    "lastName": "Kramer",
+    "birthdayDate": "08/05/1998",
+    "address": "707 Franklin Avenue - Cloverdale, Wisconsin, St. Pierre and Miquelon.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 3,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 92106-0392",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c6212cfec6a6e31ccba",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 1,
+        "price": "R$ 4.768,51",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62395369341df55c92",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 0.136,70",
+        "tag": "garden",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62f6e2eb9f09473fa8",
+    "firstName": "Mildred",
+    "lastName": "Rush",
+    "birthdayDate": "08/05/1998",
+    "address": "866 Milford Street - Hoehne, Connecticut, Azerbaijan.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 5,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 93221-9259",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62e395c1060da15867",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 6.812,98",
+        "tag": "garden",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62ed351ccda3fd3a7f",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 3,
+        "price": "R$ 5.557,59",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62c3979f2ef00083f8",
+    "firstName": "Dae",
+    "lastName": "Allen",
+    "birthdayDate": "08/05/1998",
+    "address": "344 Willow Place - Fostoria, Puerto Rico, Mauritania.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 0,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 97656-2486",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62feb3e056f5cc2eac",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 1,
+        "price": "R$ 1.040,91",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c625c16ad8b86cbee11",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 6.529,45",
+        "tag": "garden",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c629a8733f035968659",
+    "firstName": "Mildred",
+    "lastName": "Robbins",
+    "birthdayDate": "06/12/1986",
+    "address": "295 Scholes Street - Duryea, Tennessee, East Timor.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 4,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 93056-1135",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c627361281936d3d50a",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 7.894,70",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c622390a1aa6c4d9494",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 0.306,91",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62ffad5178d50f5bba",
+    "firstName": "Tandy",
+    "lastName": "Harding",
+    "birthdayDate": "08/05/1998",
+    "address": "675 Poly Place - Whitehaven, Maine, Pakistan.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 2,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 92983-2716",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c622a7cabf274e26200",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 1,
+        "price": "R$ 7.035,35",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c626890fbf115cbc811",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 1,
+        "price": "R$ 7.386,73",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62f69c5d5257f35bde",
+    "firstName": "Shiloh",
+    "lastName": "Kline",
+    "birthdayDate": "27/07/1976",
+    "address": "78 Norwood Avenue - Elbert, North Carolina, Latvia.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 2,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 94576-3791",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c6279b5a16a9bab28dd",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 0.925,17",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c620f085e7c2735840b",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 7.950,27",
+        "tag": "garden",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62488c110c12a2274c",
+    "firstName": "Farren",
+    "lastName": "Kelly",
+    "birthdayDate": "03/10/1990",
+    "address": "236 Heath Place - Idledale, California, Bahrain.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 2,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 97578-8527",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c6259505c8928aef438",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 2,
+        "price": "R$ 1.406,94",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c623c4a10c964751361",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 2.872,34",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62c5d236b9588af3a9",
+    "firstName": "Rowan",
+    "lastName": "Burke",
+    "birthdayDate": "08/05/1998",
+    "address": "359 Norman Avenue - Montura, Virgin Islands, Spain.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 1,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 95929-5654",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62dbca9349b1412ac4",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 0.151,70",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c625828f4cfda11e2d4",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 0.773,18",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62e30bcc6369aa49d7",
+    "firstName": "Riley",
+    "lastName": "Guthrie",
+    "birthdayDate": "06/12/1986",
+    "address": "644 Milford Street - Hoehne, Connecticut, Azerbaijan.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 5,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 95292-4429",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c6221c2249a310a2e52",
+        "title": "Tondre Pelouse",
+        "description": "Lorem ipsum",
+        "status": 2,
+        "price": "R$ 6.960,65",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c626f0f4d528ad9e0a4",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 9.478,15",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c624b976889a67dcfc4",
+    "firstName": "Charity",
+    "lastName": "Rush",
+    "birthdayDate": "08/05/1998",
+    "address": "343 Fiske Place - Eagleville, Massachusetts, Barbados.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 3,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 98028-1856",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c622f8c8d265c6ec950",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 3,
+        "price": "R$ 0.451,49",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62b7c00aadcd29163c",
+        "title": "Tondre Pelouse",
+        "description": "Lorem ipsum",
+        "status": 1,
+        "price": "R$ 8.825,88",
+        "tag": "garden",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c6288d6c3dcbc7d4b2a",
+    "firstName": "Kaylor",
+    "lastName": "Hutchinson",
+    "birthdayDate": "27/07/1976",
+    "address": "572 Grant Avenue - Matthews, New Jersey, Zambia.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 5,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 98367-5577",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c622463467159b87879",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 2,
+        "price": "R$ 7.139,53",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62b2ba11a617f8fde6",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 5,
+        "price": "R$ 2.356,75",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c622c1af98a19e22201",
+    "firstName": "Duffy",
+    "lastName": "Harding",
+    "birthdayDate": "03/10/1990",
+    "address": "396 Grant Avenue - Matthews, New Jersey, Zambia.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 0,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 96020-8281",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62599ffe917f6940b3",
+        "title": "Tondre Pelouse",
+        "description": "Lorem ipsum",
+        "status": 5,
+        "price": "R$ 7.771,93",
+        "tag": "stuff",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c6261b09f4148d8d0f1",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 7.956,27",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62d0fab162e1ee1e64",
+    "firstName": "Hill",
+    "lastName": "Webster",
+    "birthdayDate": "27/07/1976",
+    "address": "218 Drew Street - Klondike, Federated States Of Micronesia, Vatican City State (Holy See).",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 0,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "Admin",
+    "phone": "+55 (83) 90685-9816",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62745b798ba3a97662",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 5.361,68",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c624a907738d4c12fc3",
+        "title": "Vide Grenier",
+        "description": "Lorem ipsum",
+        "status": 5,
+        "price": "R$ 8.898,90",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62b10623fcfd4c99a7",
+    "firstName": "Taylor",
+    "lastName": "Foreman",
+    "birthdayDate": "06/12/1986",
+    "address": "737 Keen Court - Stockdale, Minnesota, Malaysia.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 1,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 93873-9424",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c62bcbc2f87ef81c1a7",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 3,
+        "price": "R$ 4.044,40",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62520e7cc4f0761278",
+        "title": "Tondre Pelouse",
+        "description": "Lorem ipsum",
+        "status": 0,
+        "price": "R$ 4.722,79",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  },
+  {
+    "id": "627a1c62a63cfd9faa9eabad",
+    "firstName": "Parker",
+    "lastName": "Sexton",
+    "birthdayDate": "03/10/1990",
+    "address": "634 Cove Lane - Crucible, Vermont, Aruba.",
+    "postalCode": "postalCode",
+    "city": "city",
+    "rate": 5,
+    "profilPicture": "https://randomuser.me/api/portraits/women/67.jpg",
+    "role": "User",
+    "phone": "+55 (83) 96688-7468",
+    "mail": "exemple@maiil.com",
+    "missions": [
+      {
+        "id": "627a1c627a58b9b858bd3020",
+        "title": "Gardechien",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 9.849,30",
+        "tag": "shopping",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      },
+      {
+        "id": "627a1c62de0d28d9d21d0fb8",
+        "title": "Faire des courses",
+        "description": "Lorem ipsum",
+        "status": 4,
+        "price": "R$ 0.703,73",
+        "tag": "dog",
+        "creationDate": "DateTime",
+        "missionDate": "DateTime"
+      }
+    ]
+  }
+]
