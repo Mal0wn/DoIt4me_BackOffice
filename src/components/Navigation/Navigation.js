359fcc7d9209cf4909bd81be8ce483cfb65e10a8
Marine M <malomclt@gmail.com>
2022-05-10 11:44:22 +0200

    Add Navigation regroup Header / Navbar

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+import NavBar from "../NavBar/NavBar";
+
+export const Navigation = () => {
+    return (
+        <div>
+            <Header/>
+            <NavBar/>
+        </div>
+    )
+}
