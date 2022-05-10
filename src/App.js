dc86eb6ee445a7b7e23de56b4e62aa31173cb9fb
Marine M <malomclt@gmail.com>
2022-05-10 11:47:31 +0200

    call navigation in app.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 import Header from './components/Header/Header'
 import NavBar from "./components/NavBar/NavBar";
 import UsersList from "./pages/Users/UsersList";
+import ItemListUser from "./pages/Users/UsersList";
+import {Rating} from "./components/Rating/Rating";
+import {Navigation} from "./components/Navigation/Navigation";
 
 
 function App() {
   return (
     <div className="App">
-        <div id="block">
-            <Header/>
-            <NavBar/>
-
-        </div>
-
-
-
-
-
-
+        <Navigation/>
     </div>
   );
 }
