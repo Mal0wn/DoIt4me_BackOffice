8151a087bef74c5ddb0f1e0bc3918440db7caaaa
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-09-23 13:45:58 +0200

    feat(service): getAllUsers added in userService

--- src/utils/services/UserService.js
+++ src/utils/services/UserService.js
@@ -1,18 +1,25 @@
+import React from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { API_BASE_URL } from '../../lib/globalVar';
 
-export default class UserService {
+export default class UserService extends React.Component {
 
   token = localStorage.getItem("accessToken");
   navigate = useNavigate();
 
-  getAllUsers() {
-    return axios.get(API_BASE_URL + 'Admin/GetAllUsers', {
-      headers: {
-        Authorization: "Bearer " + this.token ,
-      }
+  async getAllUsers(setAllUsers) {        
+      await axios.get(API_BASE_URL + "Admin/GetAllUsers", {
+        headers: {
+            Authorization: "Bearer " + this.token
+        },
     })  
+    .then(function (response) {
+      setAllUsers(response.data)
+    })
+    .catch(function (error) {
+        console.error(error);
+    })
   }
 
   /**
