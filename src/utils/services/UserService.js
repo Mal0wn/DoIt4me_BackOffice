0798b2c00f44a8574ac6a5d50872f447a8407755
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-09-23 13:01:34 +0200

    refactor(service): getCurrentUser added + API_BASE_URL imported

--- src/utils/services/UserService.js
+++ src/utils/services/UserService.js
@@ -1,17 +1,36 @@
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from '../../lib/globalVar';
 
 export default class UserService {
 
-domaine = `https://localhost:7102/api/Admin/`;
-token = localStorage.getItem("accessToken");
+  token = localStorage.getItem("accessToken");
+  navigate = useNavigate();
 
-getAllUsers() {
-	return axios.get(this.domaine + 'GetAllUsers', {
+  getAllUsers() {
+    return axios.get(API_BASE_URL + 'Admin/GetAllUsers', {
       headers: {
         Authorization: "Bearer " + this.token ,
       }
     })  
+  }
 
-}
+  /**
+   * Get CurrentUser infos
+   */
+    async getCurrentUser () {
+    await axios
+    .get(API_BASE_URL + "User/GetCurrentUser", {
+        headers: {
+            Authorization: "Bearer " + this.token
+            },
+        })
+        .then((res) => {
+            localStorage.setItem("currentUser", JSON.stringify(res.data))
+            console.log(res.data)
+            this.navigate("/users");
+        })
+    .catch((err) => console.log(err));
+  };
 };
 
