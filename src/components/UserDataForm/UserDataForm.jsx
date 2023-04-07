df2048b1419b919277bf3b0f49ca4c09d90bc187
Nicolas De waegenaere <de_waegenaere@live.fr>
2023-04-07 16:45:44 +0200

    feat(component): address management on update

--- src/components/UserDataForm/UserDataForm.jsx
+++ src/components/UserDataForm/UserDataForm.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import style from "./UserDataForm.module.css";
 import UserService from "../../utils/services/UserService.js";
+import SuccessMessage from "../UI/SuccessMessage/SuccessMessage";
+import ErrorMessage from "../UI/ErrorMessage/ErrorMessage";
 
 export const UserDataForm = ( currentUser ) => {
 
@@ -13,6 +15,13 @@ export const UserDataForm = ( currentUser ) => {
     const [email, setemail] = useState(currentUser.email);
     const [phone, setphone] = useState(currentUser.phone);
     const [birthday, setbirthday] = useState(currentUser.birthday);
+    
+    const [number, setnumber] = useState(currentUser.address && currentUser.address.length > 0 ? currentUser.address[0].number : "");
+    const [street, setstreet] = useState(currentUser.address && currentUser.address.length > 0 ? currentUser.address[0].street : "");
+    const [zipCode, setzipCode] = useState(currentUser.address && currentUser.address.length > 0 ? currentUser.address[0].zip_code : "");
+    const [city, setcity] = useState(currentUser.address && currentUser.address.length > 0 ? currentUser.address[0].city : "");
+    const [country, setcountry] = useState(currentUser.address && currentUser.address.length > 0 ? currentUser.address[0].country : "");
+
 
     const [modify, setModify] = useState(false);
     
@@ -28,10 +37,18 @@ export const UserDataForm = ( currentUser ) => {
         currentUser.lastname = lastname ? lastname : currentUser.lastname;
         currentUser.email = email ? email : currentUser.email;
         currentUser.phone = phone ? phone : currentUser.phone;
-        currentUser.birthday = birthday ? birthday : currentUser.birthday;        
+        currentUser.birthday = birthday ? birthday : currentUser.birthday;
+
+        currentUser.address[0].number = number ? number : currentUser.address[0].number;
+        currentUser.address[0].street = street ? street : currentUser.address[0].street;
+        currentUser.address[0].zip_code = zipCode ? zipCode : currentUser.address[0].zip_code;
+        currentUser.address[0].city = city ? city : currentUser.address[0].city;
+        currentUser.address[0].country = country ? country : currentUser.address[0].country;
+        
 
         try {
-            await userService.updateCurrentUser(currentUser);
+            let response = await userService.updateCurrentUser(currentUser);
+            console.log(response.status);
             setSuccessMessage("Votre profil a bien été mis à jour");
             setTimeout(() => {
                 setSuccessMessage("");
@@ -43,19 +60,18 @@ export const UserDataForm = ( currentUser ) => {
             }, 5000);
         }
     }
-
     // format birthday
     let formatBirthday = currentUser.birthday ? currentUser.birthday.split("T")[0] : "";
+    let parts = formatBirthday.split('-');
+    let reversedDate = parts.reverse().join('-');
 
     return (
         <>
             {
                 modify ? (
                     <div className={style.identityUser}>
-                        <div>
-                            {successMessage && <p className={style.success}>{successMessage}</p>}
-                            {errorMessage && <p className={style.error}>{errorMessage}</p>}
-                        </div>
+                        {successMessage && <SuccessMessage message={successMessage} />}
+                        {errorMessage && <ErrorMessage message={errorMessage} />}
                         <form method="post" onSubmit={IdentitySubmit}>
                             <div>
                                 <label>
@@ -81,6 +97,32 @@ export const UserDataForm = ( currentUser ) => {
                                 <label>
                                     <h3>Date de naissance:</h3>
                                     <input type="date" name="birthday" placeholder={formatBirthday} onChange={e => setbirthday(e.target.value)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <h3>Numéro</h3>
+                                    <input type="text" name="number" placeholder={currentUser.address[0]?.number ?? ""} onChange={e => setnumber(e.target.value)} />
+                                </label>
+                                <label>
+                                    <h3>Rue</h3>
+                                    <input type="text" name="street" placeholder={currentUser.address[0]?.street ?? ""} onChange={e => setstreet(e.target.value)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <h3>Code postal</h3>
+                                    <input type="text" name="zipCode" placeholder={currentUser.address[0]?.zip_code ?? ""} onChange={e => setzipCode(e.target.value)} />
+                                </label>
+                                <label>
+                                    <h3>Ville</h3>
+                                    <input type="text" name="city" placeholder={currentUser.address[0]?.city ?? ""} onChange={e => setcity(e.target.value)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <h3>Pays</h3>
+                                    <input type="text" name="country" placeholder={currentUser.address[0]?.country ?? ""} onChange={e => setcountry(e.target.value)} />
                                 </label>
                             </div>
                             <input type="submit" value="Mettre à jour" />
@@ -112,9 +154,44 @@ export const UserDataForm = ( currentUser ) => {
                         <div>
                             <label>
                                 <h3>Date de naissance:</h3>
-                                <p>{formatBirthday}</p>
+                                <p>{reversedDate}</p>
                             </label>
                         </div>
+                            {currentUser && currentUser.address ? (
+                                <>
+                                    <div>
+                                        <label>
+                                            <h3>Numéro</h3>
+                                            <p>{currentUser.address[0].number}</p>
+                                        </label>
+                                        <label>
+                                            <h3>Rue</h3>
+                                            <p>{currentUser.address[0]?.street}</p>
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label>
+                                            <h3>Code postal</h3>
+                                            <p>{currentUser.address[0].zip_code}</p>
+                                        </label>
+                                        <label>
+                                            <h3>Ville</h3>
+                                            <p>{currentUser.address[0].city}</p>
+                                        </label>
+                                        <label>
+                                            <h3>Pays</h3>
+                                            <p>{currentUser.address[0].country}</p>
+                                        </label>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    {console.log(currentUser.address)}
+                                    <label>
+                                        <p>L'adresse n'est pas renseignée.</p>
+                                    </label>
+                                </>
+                            )}
                         <button onClick={() => setModify(true)}>Modifier</button>                        
                     </div>
                 )}
